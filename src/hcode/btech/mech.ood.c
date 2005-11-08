a113f5032e80c7e0d8c33b2eb4b835ef08078258
gtaylor <gtaylor@e60b412a-5d77-420b-80c2-6cef64185626>
2005-11-08 05:21:44 +0000

    Ported over Exile OOD modifications.
    git-svn-id: svn://svn.code.sf.net/p/btonline-btech/code/btmux/trunk@282 e60b412a-5d77-420b-80c2-6cef64185626

--- src/hcode/btech/mech.ood.c
+++ src/hcode/btech/mech.ood.c
@@ -19,7 +19,10 @@
 #include "p.mech.utils.h"
 #include "p.mech.update.h"
 #include "p.mech.restrict.h"
+#include "p.mech.combat.h"
+#include "p.mech.damage.h"
 #include "p.template.h"
+#include "p.btechstats.h"
 
 void mech_ood_damage(MECH * wounded, MECH * attacker, int damage)
 {
@@ -53,6 +56,7 @@ void mech_ood_damage(MECH * wounded, MECH * attacker, int damage)
 void mech_ood_event(MUXEVENT * e)
 {
     MECH *mech = (MECH *) e->data;
+    int mof = 0, roll, roll_needed, para = 0;
 
     if (!OODing(mech))
 	return;
@@ -64,23 +68,82 @@ void mech_ood_event(MUXEVENT * e)
 	return;
     }
     /* Time to hit da ground */
+
+    if (Fallen(mech))
+        mof = -10;
+    if (Uncon(mech) || !Started(mech) || Blinded(mech) || MechAutoFall(mech))
+	mof = -20;
+    roll = Roll();
+    roll_needed = MechType(mech) == CLASS_BSUIT || MechType(mech) == CLASS_MW ? FindPilotPiloting(mech) - 1 : FindSPilotPiloting(mech) + MechPilotSkillBase(mech);
+    
+    if (!Started(mech))
+        roll_needed += 10;
+    if (MechCocoon(mech) == 1) {
+	para = 1;
+    } else if (MechCocoon(mech) < 0) {
+	roll_needed += 4;
+    } else if (MechCocoon(mech) == 0) {
+	roll_needed += 10;
+    }
+
+    if (MechRTerrain(mech) != GRASSLAND && MechRTerrain(mech) != ROAD) {
+        if (MechRTerrain(mech) == WATER || MechRTerrain(mech) == HIGHWATER)
+	    roll_needed += 2;
+        else
+	    roll_needed += 3;
+    }
+    
     MechCocoon(mech) = 0;
-    mech_notify(mech, MECHALL, "You land on the ground!");
-    if (!MadePilotSkillRoll(mech, 4)) {
+
+    if (In_Character(mech->mynum) && Location(MechPilot(mech)) != mech->mynum)
+        roll_needed += 99;
+    
+    mech_notify(mech, MECHPILOT, "You make a piloting skill roll!");
+    mech_notify(mech, MECHPILOT,
+        tprintf("Modified Pilot Skill: BTH %d\tRoll: %d", roll_needed, roll)); 
+    mof += (roll - roll_needed); 
+    if (roll >= roll_needed) {
+        if (roll_needed > 2)
+            AccumulatePilXP(MechPilot(mech), mech, BOUNDED(1, (abs(mof) + 1) * 2, 20), 1);
+    }
+    mof += (roll - roll_needed);
+
+    if (mof < 0) {
 	if (MechType(mech) == CLASS_MECH) {
 	    mech_notify(mech, MECHALL,
 		"You are unable to control your momentum and fall on your face!");
 	    MechLOSBroadcast(mech,
 		"touches down on the ground, twists, and falls down!");
+  	    MechFalls(mech, (abs(mof) * (para ? 1 : 2)), 1);
+	} else if (MechType(mech) == CLASS_BSUIT) {
+	    int i, ii, dam; 
+	    mech_notify(mech, MECHALL,
+	        "You are unable to control your momentum and crash!");
+	    MechLOSBroadcast(mech, "crashes to the ground!");
+	    for (i = 0; i < NUM_SECTIONS; i++) {
+			dam = 0;
+			if (GetSectOInt(mech, i) > 0) {
+				for (ii = mof; ii < 0; ii++)
+					dam += Number(1,4);
+				DamageMech(mech, mech, 0, -1, i, 0, 0, dam, -1, -1, 0, 0, 0, 0);
+				MechFloods(mech);
+			}
+		}
+		MechFalls(mech, 0, 1);
 	} else {
 	    mech_notify(mech, MECHALL,
 		"You are unable to control your momentum and crash!");
 	    MechLOSBroadcast(mech, "crashes at the ground!");
-
+		MechFalls(mech, (abs(mof) * (para ? 1 : 3)), 1);
 	}
-	MechFalls(mech, 1, 0);
-    } else
+    } else if (!para) {
 	MechLOSBroadcast(mech, "touches down!");
+    } else if (para) {
+	MechLOSBroadcast(mech, "touches down and rolls on the ground!");
+/*	mech_notify(mech, MECHALL,
+	    "As you hit the ground you roll and sponge some damage!");
+	MechFalls(mech, (MechTons(mech) / 25), 0); */
+    }
     DropSetElevation(mech, 1);
     if (!Fallen(mech))
 	domino_space(mech, 2);
