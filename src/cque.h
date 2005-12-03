53267200f2162b561b6b5e5edf7a07a470b04b57
murrayma <murrayma@e60b412a-5d77-420b-80c2-6cef64185626>
2005-12-03 04:58:48 +0000

    added two new necessary include files
    git-svn-id: svn://svn.code.sf.net/p/btonline-btech/code/btmux/trunk@315 e60b412a-5d77-420b-80c2-6cef64185626

---
+++ src/cque.h
@@ -0,0 +1,42 @@
+/* cque.h */
+/* $Id: */
+
+#ifndef __CQUE_H__
+#define __CQUE_H__
+#include "config.h"
+
+/* BQUE - Command queue */
+
+typedef struct bque BQUE;
+struct bque {
+    BQUE *next;
+    BQUE *next_wait;
+    BQUE *next_blocking;
+    
+    dbref player;       /* player who will do command */
+    dbref cause;        /* player causing command (for %N) */
+    dbref sem;          /* blocking semaphore */
+    int waittime;       /* time to run command */
+    int attr;           /* blocking attribute */
+    char *text;         /* buffer for comm, env, and scr text */
+    char *comm;         /* command */
+    char *env[NUM_ENV_VARS];    /* environment vars */
+    char *scr[NUM_ENV_VARS];    /* temp vars */
+    int nargs;          /* How many args I have */
+    struct event ev;   /* event structure for wait queue */
+};
+
+/* Per object run queues */
+typedef struct objqe OBJQE;
+
+struct objqe {
+    dbref obj;
+    BQUE *cque;
+    BQUE *ctail;
+    BQUE *wait_que; // commands waiting on this object
+    BQUE *pending_que; // obj's commands that are waiting
+    struct objqe *next;
+    int queued;
+};  
+
+#endif
---
+++ src/pcache.h
@@ -0,0 +1,28 @@
+/*
+ * pcache.h
+ */
+
+#ifndef __PCACHE_H__
+#define __PCACHE_H__
+#include "db.h"
+#include "cque.h"
+#include "rbtree.h"
+
+typedef struct player_cache {
+    dbref player;
+    int money;
+    int queue;
+    int qmax;
+    int cflags;
+    struct player_cache *next;
+} PCACHE;
+
+rbtree pcache_tree;
+PCACHE *pcache_head;
+
+#define PF_DEAD     0x0001
+#define PF_REF      0x0002
+#define PF_MONEY_CH 0x0004
+#define PF_QMAX_CH  0x0008
+
+#endif
