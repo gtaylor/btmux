755dda299acc29ec821e99003e815fe644f0decb
dany <dany@e60b412a-5d77-420b-80c2-6cef64185626>
2006-01-26 14:58:12 +0000

    Added some more checks to the dllist
    git-svn-id: svn://svn.code.sf.net/p/btonline-btech/code/btmux/trunk@535 e60b412a-5d77-420b-80c2-6cef64185626

--- src/dllist.c
+++ src/dllist.c
@@ -12,53 +12,56 @@
 dllist *dllist_create_list()
 {
 
-	dllist *temp;
+    dllist *temp;
 
-	temp = malloc(sizeof(dllist));
-	if(temp == NULL) {
-		return NULL;
-	}
+    temp = malloc(sizeof(dllist));
+    if (temp == NULL) {
+        return NULL;
+    }
 
-	memset(temp, 0, sizeof(temp));
-	temp->head = NULL;
-	temp->tail = NULL;
-	temp->size = 0;
+    memset(temp, 0, sizeof(temp));
+    temp->head = NULL;
+    temp->tail = NULL;
+    temp->size = 0;
 
-	return temp;
+    return temp;
 }
 
 /* Create a node */
 dllist_node *dllist_create_node(void *data)
 {
 
-	dllist_node *temp;
+    dllist_node *temp;
 
-	temp = malloc(sizeof(dllist_node));
-	if(temp == NULL) {
-		return NULL;
-	}
+    temp = malloc(sizeof(dllist_node));
+    if (temp == NULL) {
+        return NULL;
+    }
 
-	memset(temp, 0, sizeof(dllist_node));
-	temp->prev = NULL;
-	temp->next = NULL;
-	temp->data = data;
+    memset(temp, 0, sizeof(dllist_node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    temp->data = data;
 
-	return temp;
+    return temp;
 }
 
 /* Destroy the List - Won't destroy unless the list is empty */
 int dllist_destroy_list(dllist * dllist)
 {
 
-	/* Check the size */
-	if(dllist->size != 0) {
-		return 0;
-	} else {
-		dllist->head = NULL;
-		dllist->tail = NULL;
-		free(dllist);
-		return 1;
-	}
+    if (!dllist)
+        return 1;
+
+    /* Check the size */
+    if (dllist->size != 0) {
+        return 0;
+    } else {
+        dllist->head = NULL;
+        dllist->tail = NULL;
+        free(dllist);
+        return 1;
+    }
 
 }
 
@@ -66,91 +69,122 @@ int dllist_destroy_list(dllist * dllist)
 void *dllist_destroy_node(dllist_node * node)
 {
 
-	void *data;
+    void *data;
 
-	data = node->data;
-	node->prev = NULL;
-	node->next = NULL;
-	node->data = NULL;
-	free(node);
+    if (!node)
+        return NULL;
 
-	return data;
+    data = node->data;
+    node->prev = NULL;
+    node->next = NULL;
+    node->data = NULL;
+    free(node);
+
+    return data;
 
 }
 
 /* Insert a given node after a node */
 void dllist_insert_after(dllist * dllist, dllist_node * node,
-						 dllist_node * newnode)
+        dllist_node * newnode)
 {
 
-	newnode->prev = node;
-	newnode->next = node->next;
-	if(node->next == NULL) {
-		dllist->tail = newnode;
-	} else {
-		(node->next)->prev = newnode;
-	}
-	node->next = newnode;
+    /* Bad node to insert */
+    if (!node) {
+        return;
+    }
 
-	/* Increment */
-	dllist->size++;
+    /*! \todo {Add check here incase the dllist is bad?} */
 
-	return;
+    newnode->prev = node;
+    newnode->next = node->next;
+    if (node->next == NULL) {
+        dllist->tail = newnode;
+    } else {
+        (node->next)->prev = newnode;
+    }
+    node->next = newnode;
+
+    /* Increment */
+    dllist->size++;
+
+    return;
 }
 
 /* Insert a given node before a node */
 void dllist_insert_before(dllist * dllist, dllist_node * node,
-						  dllist_node * newnode)
+        dllist_node * newnode)
 {
 
-	newnode->prev = node->prev;
-	newnode->next = node;
-	if(node->prev == NULL) {
-		dllist->head = newnode;
-	} else {
-		(node->prev)->next = newnode;
-	}
-	node->prev = newnode;
+    /* Bad node to insert */
+    if (!node) {
+        return;
+    }
 
-	/* Increment */
-	dllist->size++;
+    /*! \todo {Add check here incase the dllist is bad?} */
 
-	return;
+    newnode->prev = node->prev;
+    newnode->next = node;
+    if (node->prev == NULL) {
+        dllist->head = newnode;
+    } else {
+        (node->prev)->next = newnode;
+    }
+    node->prev = newnode;
+
+    /* Increment */
+    dllist->size++;
+
+    return;
 }
 
 /* Insert a node at the beginning */
 void dllist_insert_beginning(dllist * dllist, dllist_node * newnode)
 {
 
-	/* If there is no head it means empty list */
-	if(dllist->head == NULL) {
-		dllist->head = newnode;
-		dllist->tail = newnode;
-		newnode->prev = NULL;
-		newnode->next = NULL;
+    /* Bad node to insert */
+    if (!newnode) {
+        return;
+    }
 
-		/* Increment */
-		dllist->size++;
+    /*! \todo {Add check here incase dllist is bad */
 
-	} else {
-		dllist_insert_before(dllist, dllist->head, newnode);
-	}
+    /* If there is no head it means empty list */
+    if (dllist->head == NULL) {
+        dllist->head = newnode;
+        dllist->tail = newnode;
+        newnode->prev = NULL;
+        newnode->next = NULL;
 
-	return;
+        /* Increment */
+        dllist->size++;
+
+    } else {
+        dllist_insert_before(dllist, dllist->head, newnode);
+    }
+
+    return;
 }
 
 /* Insert a node at the end of the list */
 void dllist_insert_end(dllist * dllist, dllist_node * newnode)
 {
 
-	/* If there is no tail means empty list */
-	if(dllist->tail == NULL) {
-		dllist_insert_beginning(dllist, newnode);
-	} else {
-		dllist_insert_after(dllist, dllist->tail, newnode);
-	}
+    /* Bad node to insert */
+    if (!newnode) {
+        return;
+    }
 
-	return;
+    /*! \todo {Add a check here incase the list is bad?} */
+
+    /* If there is no tail means empty list */
+    if (dllist->tail == NULL) {
+        dllist_insert_beginning(dllist, newnode);
+    } else {
+        dllist_insert_after(dllist, dllist->tail, newnode);
+    }
+
+    return;
 
 }
 
@@ -158,29 +192,53 @@ void dllist_insert_end(dllist * dllist, dllist_node * newnode)
 void *dllist_remove(dllist * dllist, dllist_node * node)
 {
 
-	void *data;
+    void *data;
 
-	/* We're checking if this first node */
-	if(node->prev == NULL) {
-		dllist->head = node->next;
-	} else {
-		(node->prev)->next = node->next;
-	}
+    /* Invalid node? */
+    if (!node) {
+        return NULL;
+    }
 
-	/* Check if end of list */
-	if(node->next == NULL) {
-		dllist->tail = node->prev;
-	} else {
-		(node->next)->prev = node->prev;
-	}
+    /* Invalid list? */
+    if (!dllist) {
 
-	/* Destroy Node */
-	data = dllist_destroy_node(node);
+        /* Try and return the data */
+        data = dllist_destroy_node(node);
+        return data;
+    }
 
-	/* De-Increment */
-	dllist->size--;
+    /*! \todo {Maybe add a check based on dllist->size? this might cause
+     * problems if the list is still linked to something} */
 
-	return data;
+    /* Somehow the list has nothing in it yet it thinks it does */
+    if (dllist->head == NULL && dllist->tail == NULL) {
+
+        /* Try and return the data */
+        data = dllist_destroy_node(node);
+        return data;
+    }
+
+    /* We're checking if this first node */
+    if (node->prev == NULL) {
+        dllist->head = node->next;
+    } else {
+        (node->prev)->next = node->next;
+    }
+
+    /* Check if end of list */
+    if (node->next == NULL) {
+        dllist->tail = node->prev;
+    } else {
+        (node->next)->prev = node->prev;
+    }
+
+    /* Destroy Node */
+    data = dllist_destroy_node(node);
+
+    /* De-Increment */
+    dllist->size--;
+
+    return data;
 
 }
 
@@ -188,28 +246,32 @@ void *dllist_remove(dllist * dllist, dllist_node * node)
 void *dllist_remove_node_at_pos(dllist * dllist, int pos)
 {
 
-	int counter = 1;
-	dllist_node *temp;
-	void *data;
+    int counter = 1;
+    dllist_node *temp;
+    void *data;
 
-	if(dllist_size(dllist) < pos) {
-		return NULL;
-	}
+    if (!dllist) {
+        return NULL;
+    }
 
-	/* Start at the head */
-	temp = dllist_head(dllist);
+    if (dllist_size(dllist) < pos) {
+        return NULL;
+    }
 
-	while (counter != pos) {
+    /* Start at the head */
+    temp = dllist_head(dllist);
 
-		temp = dllist_next(temp);
-		counter++;
+    while (counter != pos) {
 
-	}
+        temp = dllist_next(temp);
+        counter++;
 
-	/* Remove the node */
-	data = dllist_remove(dllist, temp);
+    }
 
-	return data;
+    /* Remove the node */
+    data = dllist_remove(dllist, temp);
+
+    return data;
 
 }
 
@@ -218,69 +280,92 @@ void *dllist_remove_node_at_pos(dllist * dllist, int pos)
 /* Get Head node */
 dllist_node *dllist_head(dllist * dllist)
 {
-	return dllist->head;
+
+    if (!dllist)
+        return NULL;
+
+    return dllist->head;
 }
 
 /* Get Tail Node */
 dllist_node *dllist_tail(dllist * dllist)
 {
-	return dllist->tail;
+    
+    if (!dllist)
+        return NULL;
+
+    return dllist->tail;
 }
 
 /* Gets next node */
 dllist_node *dllist_next(dllist_node * node)
 {
-	return node->next;
+
+    if (!node)
+        return NULL;
+
+    return node->next;
 }
 
 /* Gets previous node */
 dllist_node *dllist_prev(dllist_node * node)
 {
-	return node->prev;
+    if (!node)
+        return NULL;
+
+    return node->prev;
 }
 
 /* Returns the data for the node */
 void *dllist_data(dllist_node * node)
 {
-	return node->data;
+
+    if (!node)
+        return NULL;
+
+    return node->data;
 }
 
 /* Get the size of the list */
 int dllist_size(dllist * dllist)
 {
 
-	if(!(dllist)) {
-		return 0;
-	}
+    if (!dllist) {
+        return 0;
+    }
 
-	return dllist->size;
+    return dllist->size;
 }
 
 /* Get the data from the Node in the List at Pos # */
 void *dllist_get_node(dllist * dllist, int pos)
 {
 
-	int counter = 1;
-	dllist_node *temp;
+    int counter = 1;
+    dllist_node *temp;
 
-	if(dllist_size(dllist) < pos) {
-		return NULL;
-	}
+    if (!dllist) {
+        return NULL;
+    }
 
-	if(pos < counter) {
-		return NULL;
-	}
+    if (dllist_size(dllist) < pos) {
+        return NULL;
+    }
 
-	/* Start at the head */
-	temp = dllist_head(dllist);
+    if (pos < counter) {
+        return NULL;
+    }
 
-	while (counter != pos) {
+    /* Start at the head */
+    temp = dllist_head(dllist);
 
-		temp = dllist_next(temp);
-		counter++;
+    while (counter != pos) {
 
-	}
+        temp = dllist_next(temp);
+        counter++;
 
-	return dllist_data(temp);
+    }
+
+    return dllist_data(temp);
 
 }
