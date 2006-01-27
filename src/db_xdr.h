415cdb1fd534a5f354f18343f81a95128659d094
murrayma <murrayma@e60b412a-5d77-420b-80c2-6cef64185626>
2006-01-27 17:45:19 +0000

    oops.
    git-svn-id: svn://svn.code.sf.net/p/btonline-btech/code/btmux/trunk@544 e60b412a-5d77-420b-80c2-6cef64185626

--- src/db_xdr.h
+++ src/db_xdr.h
@@ -13,21 +13,22 @@ struct mmdb_t {
 	int fd;
 };
 
-struct mmdb_t *_open_read(char *filename);
-struct mmdb_t *_open_write(char *filename);
+struct mmdb_t *mmdb_open_read(char *filename);
+struct mmdb_t *mmdb_open_write(char *filename);
 void mmdb_resize(struct mmdb_t *, int length);
 void mmdb_close(struct mmdb_t *);
-void *_read(struct mmdb_t *, void *dest, int length);
+void *mmdb_read(struct mmdb_t *, void *dest, int length);
 void mmdb_write(struct mmdb_t *, void *data, int length);
 void mmdb_write_opaque(struct mmdb_t *, void *data, int length);
 
 void mmdb_write_uint(struct mmdb_t *, unsigned int); /* Deprecated */
 unsigned int mmdb_read_uint(struct mmdb_t *); /* Deprecated */
 
-void mmdb_write_uint8(struct mmdb_t *, uint8_t);
-uint8_t mmdb_read_uint8(struct mmdb_t *);
-void mmdb_write_uint16(struct mmdb_t *, uint16_t);
-uint16_t mmdb_read_uint16(struct mmdb_t *);
+#define mmdb_write_uint8(db,val) mmdb_write_uint32(db, (uint32_t)val);
+#define mmdb_read_uint8(db) (uint8_t)mmdb_read_uint32(db);
+#define mmdb_write_uint16(db,val) mmdb_write_uint32(db, (uint32_t)val);
+#define mmdb_read_uint16(db) (uint8_t)mmdb_read_uint32(db);
+
 void mmdb_write_uint32(struct mmdb_t *, uint32_t);
 uint32_t mmdb_read_uint32(struct mmdb_t *);
 void mmdb_write_uint64(struct mmdb_t *, uint64_t);
